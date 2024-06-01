2e50a524bba8b76df41ba2a30b5e55b1e871ff29
Abhishek Biswas <abhishek.biswas@grabtaxi.com>
2024-06-01 12:14:21 +0530

    Added few changes

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
 return {
   { "folke/tokyonight.nvim", opts = { style = "night" } },
   { "diegoulloao/neofusion.nvim", priority = 1000, config = true, opts = ... },
+  { "EdenEast/nightfox.nvim", priority = 1000, config = true, opts = ... },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "neofusion",
+      colorscheme = "carbonfox",
     },
   },
 }
